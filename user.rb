69b7d19e29d8bf02801b29a8358efeb2b28bfd77
kristiyantodorov <todorovke@gmail.com>
2016-02-11 15:35:01 +0200

    Fix #view_flights to work properly with the interface

--- user.rb
+++ user.rb
@@ -2,7 +2,10 @@ class Airport::User
   def view_all_flights
     SQLite3::Database.open 'Airport.db' do |db|
       db.execute("SELECT * FROM Flights WHERE Deleted = 'false'") do |row|
-        puts row.map{ |e| e.to_s }.first(8).join(', ')
+        flights = Array.new.tap do |f|
+          f << row.map{ |e| e.to_s }.first(8).join(', ')
+        end
+        return flights
       end
     end
   end
