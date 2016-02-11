5ec102485e29d0e855bcbbfe897e448612dac277
kristiyantodorov <todorovke@gmail.com>
2016-02-11 16:04:12 +0200

    Fix bugs in returning flights

--- guest.rb
+++ guest.rb
@@ -1,30 +1,26 @@
 class Airport::Guest < Airport::User
   def view_flights_by_attribute(direction, destination)
+    flights = []
     SQLite3::Database.open 'Airport.db' do |db|
       db.execute "SELECT * FROM Flights
                     WHERE Type = '#{direction}' AND
                           Destination = '#{destination}'" do |row|
-        flights = Array.new.tap do |f|
-          f << row.map{ |e| e.to_s }.first(8).join(', ')
-        end
-
-        return flights
+          flights << row.map{ |e| e.to_s }.first(8).join(', ')
       end
     end
+    return flights
   end
 
   def view_flights_to(destination)
+    flights = []
     SQLite3::Database.open 'Airport.db' do |db|
       db.execute "SELECT * FROM Flights
                   WHERE Destination = #{destination} AND
                         Type = 'departing'" do |row|
-        flights = Array.new.tap do |f|
-          f << row.map{ |r| r.to_s }.first(8).join(',')
-        end
-
-        return flights
+        flights << row.map{ |r| r.to_s }.first(8).join(',')
       end
     end
+    return flights
   end
 
   def check_flight_status(flight_id)
